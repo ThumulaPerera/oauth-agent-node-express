d3876afd0db05d6a51f4964783ed704c17830dd1
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2023-09-27 11:12:10 +0530

    Modify structure to support tests

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import * as express from 'express'
+import * as cors from 'cors'
+import * as cookieParser from 'cookie-parser'
+import {
+    LoginController,
+    ClaimsController,
+    LogoutController,
+    RefreshTokenController
+} from './controller'
+import {serverConfig} from './serverConfig'
+import loggingMiddleware from './middleware/loggingMiddleware'
+import exceptionMiddleware from './middleware/exceptionMiddleware'
+
+const app = express()
+const corsConfiguration = {
+    origin: serverConfig.trustedWebOrigins,
+    credentials: true,
+    methods: ['POST']
+}
+
+if (serverConfig.corsEnabled) {
+    app.use(cors(corsConfiguration))
+}
+
+app.use(cookieParser())
+app.use('*', express.json())
+app.use('*', loggingMiddleware)
+app.use('*', exceptionMiddleware)
+app.set('etag', false)
+
+const controllers = {
+    '/login': new LoginController(),
+    '/userInfo': new ClaimsController(),
+    '/logout': new LogoutController(),
+    '/refresh': new RefreshTokenController()
+}
+
+for (const [path, controller] of Object.entries(controllers)) {
+    app.use(serverConfig.endpointsPrefix + path, controller.router)
+}
+
+export default app
