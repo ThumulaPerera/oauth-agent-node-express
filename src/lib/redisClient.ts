bb7af01def95cd5c8159200bce690ab3cca7296b
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2023-09-27 11:11:17 +0530

    Modify structure to support tests

--- src/lib/redisClient.ts
+++ src/lib/redisClient.ts
@@ -1,8 +1,18 @@
-// ref https://github.com/redis/ioredis/tree/main/examples/express
+let Redis: any;
 
-import { createRedisClient } from './redisConfig';
+if (process.env.NODE_ENV === 'test') {
+  Redis = require('ioredis-mock');
+} else {
+  // ref https://github.com/redis/ioredis/tree/main/examples/express
+  Redis = require('ioredis');
+}
 
-const redisClient = createRedisClient();
+const redisClient = new Redis({
+  port: parseInt(process.env.REDIS_PORT || '6379'),
+  host: process.env.REDIS_HOST || 'localhost',
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+});
 
 const hgetallWithRetry = async (key: string, retries = 2): Promise<Record<string, string>> => {
   try {
